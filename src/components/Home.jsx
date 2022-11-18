d9b0971f2a1a3f822f12d5c56e57d65bf505e307
unknown <rayhan.senbag.bd@gmail.com>
2022-11-18 23:42:51 +0600

    facebook login added

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,14 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useBarcode } from "@createnextapp/react-barcode";
+import { useState } from "react";
+import { useRef } from "react";
+import { GoogleLogin } from "react-google-login";
+import FacebookLogin from "react-facebook-login";
+import { gapi } from "gapi-script";
 
 const Home = () => {
+  const [imgUrl, setImgUrl] = useState([]);
+  const fileInput = useRef(null);
   const { inputRef } = useBarcode({
     value: `INV ${Date.now()}`,
     options: {
@@ -21,9 +28,73 @@ const Home = () => {
     link.href = image;
     link.click();
   }
+
+  function imgHandler(file) {
+    if (file) {
+      const images = [];
+      Array.from(file).forEach((img) =>
+        images.push({ name: img.name, src: URL.createObjectURL(img) })
+      );
+      setImgUrl(images);
+    }
+  }
+  function deleteImage(name) {
+    const filtered = imgUrl.filter((img) => img.name !== name);
+    setImgUrl(filtered);
+  }
+
+  const clientId =
+    "484793000620-9o79p0cmhru9kucma8ri67hkd3h3i0mm.apps.googleusercontent.com";
+  useEffect(() => {
+    const initClient = () => {
+      gapi.auth2.init({
+        apiKey: "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",
+        clientId: clientId,
+        scope: "https://www.googleapis.com/auth/drive.metadata.readonly",
+      });
+    };
+    gapi.load("client:auth2", initClient);
+  });
+
   return (
     <div>
       <canvas onClick={downloadCanvas} id='print-area' ref={inputRef} />
+      <br />
+      <button onClick={() => fileInput.current.click()}>Choose files</button>
+      <input
+        onChange={(e) => imgHandler(e.target.files)}
+        type='file'
+        hidden
+        ref={fileInput}
+        multiple
+      />
+      <br />
+      <br />
+      {imgUrl.length
+        ? imgUrl.map((img, i) => (
+            <img
+              onClick={() => deleteImage(img.name)}
+              key={i}
+              src={img.src}
+              alt=''
+            />
+          ))
+        : null}
+      <br />
+      <GoogleLogin
+        clientId={clientId}
+        buttonText='Login'
+        onSuccess={(response) => console.log(response)}
+        onFailure={(response) => console.log(response)}
+        cookiePolicy={"single_host_origin"}
+      />
+      <FacebookLogin
+        appId='438654467748573'
+        fields='name,email,picture'
+        onClick={(response) => console.log(response)}
+        callback={(response) => console.log(response)}
+        icon='fa-facebook'
+      />
     </div>
   );
 };
